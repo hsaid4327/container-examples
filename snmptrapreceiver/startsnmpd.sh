6e5d0670ff4b52cd963c0311d001b7721720d7fd
Ed Presutti <epresutti@gmail.com>
2019-12-07 01:47:38 +0000

    Port Redirection and better output

--- snmptrapreceiver/startsnmpd.sh
+++ snmptrapreceiver/startsnmpd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. /etc/sysconfig/snmpd && /usr/sbin/snmpd &
-. /etc/sysconfig/snmptrapd && /usr/sbin/snmptrapd &
+. /etc/sysconfig/snmpd && /usr/sbin/snmpd tcp:1161 udp:1161 -Lo &
+. /etc/sysconfig/snmptrapd && /usr/sbin/snmptrapd tcp:1162 udp:1162 -Lo &
 
 tail -f /dev/null
