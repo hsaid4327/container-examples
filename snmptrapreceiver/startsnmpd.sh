9397bc2f70b902fc5b2c89196d84518b7ea85274
Ed Presutti <epresutti@gmail.com>
2019-12-08 00:56:18 +0000

    Set up SNMP Trap log and tail it.

--- snmptrapreceiver/startsnmpd.sh
+++ snmptrapreceiver/startsnmpd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 . /etc/sysconfig/snmpd && /usr/sbin/snmpd tcp:1161 udp:1161 -Lo &
-. /etc/sysconfig/snmptrapd && /usr/sbin/snmptrapd tcp:1162 udp:1162 -Lo &
+. /etc/sysconfig/snmptrapd && /usr/sbin/snmptrapd tcp:1162 udp:1162 -Lf /snmptrap.log &
 
-tail -f /dev/null
+tail -f /snmptrap.log
