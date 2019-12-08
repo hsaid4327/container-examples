0b18a045175a8b9c0cccf44282a77e44caac8341
Ed Presutti <epresutti@gmail.com>
2019-12-08 00:57:18 +0000

    Add a touch for the snmptrap.log and tail it

--- snmptrapreceiver/startsnmpd.sh
+++ snmptrapreceiver/startsnmpd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+touch /snmptrap.log
+
 . /etc/sysconfig/snmpd && /usr/sbin/snmpd tcp:1161 udp:1161 -Lo &
 . /etc/sysconfig/snmptrapd && /usr/sbin/snmptrapd tcp:1162 udp:1162 -Lf /snmptrap.log &
 
