1232d0e7592f1ab067fc5e64a01e6c2d3fb450e7
Ed Presutti <epresutti@gmail.com>
2019-12-08 01:42:01 +0000

    Doing some debugging...

--- snmptrapreceiver/startsnmpd.sh
+++ snmptrapreceiver/startsnmpd.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-touch /snmptrap.log
+#touch /tmp/snmptrap.log
 
+echo Starting snmpd
 . /etc/sysconfig/snmpd && /usr/sbin/snmpd tcp:1161 udp:1161 -Lo &
-. /etc/sysconfig/snmptrapd && /usr/sbin/snmptrapd tcp:1162 udp:1162 -Lf /snmptrap.log &
 
-tail -f /snmptrap.log
+echo Starting snmptrapd
+. /etc/sysconfig/snmptrapd && /usr/sbin/snmptrapd tcp:1162 udp:1162 -Lo &
+#. /etc/sysconfig/snmptrapd && /usr/sbin/snmptrapd tcp:1162 udp:1162 -Lf /tmp/snmptrap.log &
+
+tail -f /dev/null
